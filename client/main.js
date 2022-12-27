28fc9ef2e1f987052fef36e08362c2489846e71b
Juhee-Hwang <julia0276@naver.com>
2022-12-27 13:42:07 +0900

    first commit

---
+++ client/main.js
@@ -0,0 +1,2 @@
+console.log('Hello World')
+console.log(window) // 브라우저가 출력된다.
---
+++ server/index.js
@@ -0,0 +1,16 @@
+// console.log("hi"); // 여기서는 node를 사용해서 실행한다.
+
+// console.log(window); // 브라우저가 아니라서 에러가 발생한다.
+
+// console.log(global); // 글로벌은 노드 안에 살고 있는 객체라서 정보를 불러온다.
+
+const liveServer = require("live-server");
+
+const params = {
+  host: "localhost",
+  port: 3000,
+  open: false,
+  root: "./client",
+};
+
+liveServer.start(params);
