57f615992af1620035a738c95bd8982cf799561c
Juhee-Hwang <julia0276@naver.com>
2023-01-17 13:17:17 +0900

    [:sparkles: feat] xhrData 함수 만들기

---
+++ client/lib/utils/xhr.js
@@ -0,0 +1,56 @@
+/* 
+  readyState
+  0 : uninitialized // 초기화
+  1 : loading // 로딩
+  2 : loaded // 로딩 완료된
+  3 : interactive // 인터렉티브
+  4 : complete // 완료
+*/
+
+function xhrData(method, url, body) {
+
+  const xhr = new XMLHttpRequest();
+  
+  // ⭐️ 이거끼리 세트! open(), send() 무조건 세트
+  // ⭐️ 비동기 통신 오픈
+  xhr.open(method, url)
+  
+  xhr.addEventListener('readystatechange', () => {
+    if(xhr.status >= 200 && xhr.status < 400){
+      if (xhr.readyState === 4) {
+        console.log('통신 성공');
+        console.log(JSON.parse(xhr.response));
+      }
+    }else{
+      console.error('통신 실패');
+    }
+  })
+
+  
+  // ⭐️ 서버에 요청
+  xhr.send(JSON.stringify(body));
+}
+
+xhrData('POST', 'https://jsonplaceholder.typicode.com/users',{
+    "name": "MESSI",
+    "username": "GOAT",
+    "email": "goat@messi.psg",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  }
+)
