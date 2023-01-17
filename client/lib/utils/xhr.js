a17e58e7867a3af5e89256afda45f67233f4a91b
Juhee-Hwang <julia0276@naver.com>
2023-01-17 16:22:09 +0900

    [:sparkles: add]] 콜백 함수 활용

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -10,11 +10,12 @@
 
 // function xhrData(options) {
 
-function xhrData({
+export function xhrData({
   url = '',
   method = 'GET',
   body = null,
-  on = '',
+  onSuccess = null,
+  onFail = null,
   headers = {
     'Content-Type' : 'application/json',
     'Access-Control-Allow-Origin' : '*',
@@ -31,9 +32,10 @@ function xhrData({
   xhr.open(method, url)
 
   // 이걸 쓰는 이유는 headers 객체를 -> 배열로 -> foEach로 구조분해할당 -> key, value 값 받기
-  Object.entries(headers).forEach(([key, value]) => {
+  /*Object.entries(headers).forEach(([key, value]) => {
     xhr.setRequestHeader(key, value);
   });
+  */
   
   
   // 객체 구조 분해 할당  
@@ -54,12 +56,100 @@ function xhrData({
   xhr.send(JSON.stringify(body));
 }
 
+xhrData.get = (url, onSuccess, onFail) => {
+  xhrData({
+    url,
+    onSuccess,
+    onFail
+  })
+}
+
+xhrData.post = (url, body, onSuccess, onFail) => {
+  xhrData({
+    method:'POST',
+    body,
+    url,
+    onSuccess,
+    onFail
+  })
+}
+
+xhrData.put = (url, body, onSuccess, onFail) => {
+  xhrData({
+    method:'PUT',
+    body,
+    url,
+    onSuccess,
+    onFail
+  })
+}
+
+xhrData.delete = (url, body, onSuccess, onFail) => {
+  xhrData({
+    method:'DELETE',
+    url,
+    onSuccess,
+    onFail
+  })
+}
+
+/*
+xhrData.get(
+  'https://jsonplaceholder.typicode.com/users',
+  (result)=>{
+    console.log(result);
+  },
+  (err) => {
+    console.log(err);
+  }
+)
+*/
+
+/*
+xhrData.post(
+  'https://jsonplaceholder.typicode.com/users',
+  {
+    "name": "MESSI",
+    "username": "GOAT",
+    "email": "goat@messi.psg",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  (result)=>{
+    console.log(result);
+  },
+  (err) => {
+    console.log(err);
+  }
+)
+*/
+
+/*
 xhrData({
   url : 'https://jsonplaceholder.typicode.com/users/1',
-  onSuccess: () => {
-    
+  onSuccess: (result) => {
+    console.log(result);
+  },
+  onFail: (err) => {
+    console.error(err);
   }
 })
+*/
 
 // 객체 받아서 바로 넣기
 // xhrData({
